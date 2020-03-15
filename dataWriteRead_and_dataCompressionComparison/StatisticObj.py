d6b0232bcf7a54f401271bc9c5f73351875eeab7
JNturtle <31470045+JNturtle@users.noreply.github.com>
2020-03-15 11:40:42 +0800

    Add files via upload

---
+++ dataWriteRead_and_dataCompressionComparison/StatisticObj.py
@@ -0,0 +1,50 @@
+import os, sys
+
+class WRStatistic():
+    """用來紀錄並統計數據"""
+    
+    def __init__(self, name, fileName, data, compresslevel = None):
+        self.name = name
+        self.Wtime, self.Rtime, self.Utime = [], [], []
+        self.fileName = fileName
+        self.dataSize = sys.getsizeof(data)  
+        self.fileSize = self.dataSize
+        if compresslevel: self.compresslevel = compresslevel 
+        self.runtimes = 0
+
+    def addRecord(self, useTimer, writeTimer, readTimer, endTimer):
+        self.Wtime.append(readTimer - writeTimer - (writeTimer - useTimer))
+        self.Rtime.append(endTimer - readTimer - (writeTimer - useTimer))
+        self.Utime.append(self.Wtime[-1] + self.Rtime[-1])   
+        self.runtimes += 1
+
+    def printThisStat(self):
+        print("method: {:}, Wtime:{:.5f}, Rtime:{:.5f}, Utime:{:.5f}".format(self.name, self.Wtime[-1], self.Rtime[-1], self.Utime[-1]))
+
+    def printTotalStat(self):     
+        self.fileSize = os.path.getsize(self.fileName)
+        print("Method: {:}, Compress Ratio: {:.2f}：1".format(self.name, self.dataSize/self.fileSize))
+        print("Average Use Time: avgWtime:{:.5f}, avgRtime:{:.5f}, avgUtime:{:.5f}".format(self.avgWT, self.avgRT, self.avgUT))
+        print("Data size: {:}({:.2f}MB), Wspeed:{:.5f} MB/s, Rspeed:{:.5f} MB/s, Uspeed:{:.5f} MB/s".format(self.dataSize, self.dataSize / 1024000, self.dWspeed, self.dRspeed, self.dUspeed))
+        print("File size: {:}({:.2f}MB), Wspeed:{:.5f} MB/s, Rspeed:{:.5f} MB/s, Uspeed:{:.5f} MB/s".format(self.fileSize, self.fileSize / 1024000, self.Wspeed, self.Rspeed, self.Uspeed))
+
+    @property
+    def avgWT(self): return sum(self.Wtime) / self.runtimes
+    @property
+    def avgRT(self): return sum(self.Rtime) / self.runtimes
+    @property
+    def avgUT(self): return sum(self.Utime) / self.runtimes
+    @property
+    def dWspeed(self): return self.dataSize / self.avgWT / 1024000
+    @property
+    def dRspeed(self): return self.dataSize / self.avgRT / 1024000
+    @property
+    def dUspeed(self): return self.dataSize / self.avgUT / 1024000
+    @property
+    def Wspeed(self): return self.fileSize / self.avgWT / 1024000
+    @property
+    def Rspeed(self): return self.fileSize / self.avgRT / 1024000
+    @property
+    def Uspeed(self): return self.fileSize / self.avgUT / 1024000
+
+
---
+++ dataWriteRead_and_dataCompressionComparison/example.py
@@ -0,0 +1,178 @@
+import json, gzip, lzma, bz2, zlib
+from time import perf_counter
+from StatisticObj import WRStatistic
+
+data = [("阿明", "身體狀況", {"體重":78.9, "肝功能": "正常"}, 90, 85, 97, 81,  85, True)]*150000
+runtimes = 10
+
+# 運行主程式
+if __name__ == '__main__':
+    zlTest = WRStatistic("json zlib", "ex_zl_RWSpeed", data, compresslevel = 6)
+    sjTest = WRStatistic("str json", "ex_sj_RWSpeed.txt", data)
+    gzTest = WRStatistic("json gz", "ex_gz_RWSpeed.gz", data, compresslevel = 9)
+    xzTest = WRStatistic("json xz", "ex_xz_RWSpeed.xz", data, compresslevel = 6)
+    jjTest = WRStatistic("json json", "ex_jj_RWSpeed.txt", data)
+    sbTest = WRStatistic("bstr list", "ex_sb_RWSpeed.txt", data)
+    slTest = WRStatistic("str str (list)", "ex_sl_RWSpeed.txt", data)
+    stTest = WRStatistic("str str (tuple)", "ex_st_RWSpeed.txt", data)
+    bzTest = WRStatistic("json bz", "ex_bz_RWSpeed", data, compresslevel = 9)
+    # 在這裡擺入想執行的測試
+    run_test = [zlTest, sjTest, gzTest, xzTest]#, jjTest, sbTest, slTest, bzTest] 
+
+    def runZlib(WRS, compressLevel = 6):
+        """ json zlib"""
+        if WRS.compresslevel: compressLevel = WRS.compresslevel
+        useTimer = perf_counter()      
+        writeTimer = perf_counter() 
+        with open(WRS.fileName, 'wb') as f: 
+            f.write(zlib.compress(bytes(json.dumps(data), encoding="ascii"), level=compressLevel))
+        f.close()
+        readTimer = perf_counter()     
+        with open(WRS.fileName, "rb") as f:
+            res = json.loads(zlib.decompress(f.read()))
+        f.close()
+        endTimer = perf_counter()
+        WRS.addRecord(useTimer, writeTimer, readTimer, endTimer)
+        WRS.printThisStat()
+    def runSj(WRS):
+        """ string json """
+        useTimer = perf_counter()      
+        writeTimer = perf_counter()  
+        with open(WRS.fileName, 'w') as f:
+            f.write(json.dumps(data))
+        f.close()
+        readTimer = perf_counter()     
+        with open(WRS.fileName, 'r') as f:
+            result = json.load(f)
+        f.close()
+        endTimer = perf_counter()
+        WRS.addRecord(useTimer, writeTimer, readTimer, endTimer)
+        WRS.printThisStat()
+    def runGz(WRS, compressLevel = 9):
+        """ json gz"""
+        if WRS.compresslevel: compressLevel = WRS.compresslevel
+        useTimer = perf_counter()      
+        writeTimer = perf_counter() 
+        with open(WRS.fileName, 'wb') as f: 
+            f.write(gzip.compress(bytes(json.dumps(data), encoding="ascii"), compresslevel = compressLevel))
+        f.close()
+        readTimer = perf_counter()     
+        with gzip.open(WRS.fileName, "rb" ) as f:
+            res = json.load(f)
+        f.close()
+        endTimer = perf_counter()
+        WRS.addRecord(useTimer, writeTimer, readTimer, endTimer)
+        WRS.printThisStat()
+    def runXz(WRS, compressLevel = 6):
+        """ json xz"""
+        if WRS.compresslevel: compressLevel = WRS.compresslevel
+        useTimer = perf_counter()      
+        writeTimer = perf_counter() 
+        with lzma.open(WRS.fileName, 'wb', preset=compressLevel) as f: 
+            f.write(bytes(json.dumps(data), encoding="ascii"))
+        f.close()
+        readTimer = perf_counter()     
+        with lzma.open(WRS.fileName, 'rb') as f: 
+            res = json.load(f)
+        f.close()
+        endTimer = perf_counter()
+        WRS.addRecord(useTimer, writeTimer, readTimer, endTimer)
+        WRS.printThisStat()
+    def runJj(WRS):
+        """ json json"""
+        useTimer = perf_counter()      
+        writeTimer = perf_counter()  
+        with open(WRS.fileName, 'w') as f:
+            json.dump(data, f)
+        f.close()
+        readTimer = perf_counter()     
+        with open(WRS.fileName, 'r') as f:
+            result = json.load(f)
+        f.close()
+        endTimer = perf_counter()
+        WRS.addRecord(useTimer, writeTimer, readTimer, endTimer)
+        WRS.printThisStat()
+    def runSb(WRS):
+        """ string list"""
+        useTimer = perf_counter()
+        writeTimer = perf_counter()  
+        with open(WRS.fileName, 'wb') as f:
+            f.write(bytes(str(data), encoding = "utf8"))
+        f.close()
+        readTimer = perf_counter()         
+        with open(WRS.fileName, 'rb') as f:
+            result = list(eval(f.read()))
+        f.close()
+        endTimer = perf_counter()
+        WRS.addRecord(useTimer, writeTimer, readTimer, endTimer)
+        WRS.printThisStat()
+    def runSl(WRS):
+        """ string list"""
+        useTimer = perf_counter()
+        writeTimer = perf_counter()  
+        with open(WRS.fileName, 'w') as f:
+            f.write(str(data))
+        f.close()
+        readTimer = perf_counter()         
+        with open(WRS.fileName, 'r') as f:
+            result = list(eval(f.read()))
+        f.close()
+        endTimer = perf_counter()
+        WRS.addRecord(useTimer, writeTimer, readTimer, endTimer)
+        WRS.printThisStat()
+    def runSt(WRS):
+        """ string tuple"""
+        useTimer = perf_counter()      
+        writeTimer = perf_counter()  
+        with open(WRS.fileName, 'w') as f:
+            f.write(str(data))
+        f.close()
+        readTimer = perf_counter()     
+        with open(WRS.fileName, 'r') as f:
+            result = tuple(eval(f.read()))
+        f.close()
+        endTimer = perf_counter()
+        WRS.addRecord(useTimer, writeTimer, readTimer, endTimer)
+        WRS.printThisStat()
+    def runBz(WRS, compressLevel = 9):
+        """ json bz"""
+        if WRS.compresslevel: compressLevel = WRS.compresslevel
+        useTimer = perf_counter()      
+        writeTimer = perf_counter() 
+        with bz2.open(WRS.fileName, 'wb', compresslevel = compressLevel) as f: 
+            f.write(bytes(json.dumps(data), encoding="ascii"))
+        f.close()
+        readTimer = perf_counter()     
+        with bz2.open(WRS.fileName, 'rb') as f: 
+            res = json.load(f)
+        f.close()
+        endTimer = perf_counter()
+        WRS.addRecord(useTimer, writeTimer, readTimer, endTimer)
+        WRS.printThisStat()
+    for _ in range(runtimes):
+        for test in run_test:
+            if test is zlTest: runZlib(zlTest)
+            elif test is sjTest: runSj(sjTest)
+            elif test is gzTest: runGz(gzTest)
+            elif test is xzTest: runXz(xzTest)
+            elif test is jjTest: runJj(jjTest)
+            elif test is sbTest: runSb(sbTest)
+            elif test is slTest: runSl(slTest)
+            elif test is stTest: runSt(stTest)
+            elif test is bzTest: runBz(bzTest)
+    print("[Statistics]")
+    if zlTest.runtimes: zlTest.printTotalStat()
+    if sjTest.runtimes: sjTest.printTotalStat()
+    if gzTest.runtimes: gzTest.printTotalStat()
+    if xzTest.runtimes: xzTest.printTotalStat()
+    if jjTest.runtimes: jjTest.printTotalStat()
+    if sbTest.runtimes: sbTest.printTotalStat()
+    if slTest.runtimes: slTest.printTotalStat()
+    if stTest.runtimes: stTest.printTotalStat()
+    if bzTest.runtimes: bzTest.printTotalStat()
+
+
+
+
+
+
